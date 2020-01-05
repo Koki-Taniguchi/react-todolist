1ba975a03058031734db734e3852fe75a9dbc77d
Koki-Taniguchi <taniguchi.437@gmail.com>
2020-01-05 20:44:29 +0900

    Updates

---
+++ precache-manifest.5b8739810c94ab9967d38d099100aa91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7acd0addb76e1044d9067f6468f21aa",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "44d77526e94612057472",
+    "url": "/react-todolist/static/css/main.279d2a5d.chunk.css"
+  },
+  {
+    "revision": "40dd92e49eae7411b535",
+    "url": "/react-todolist/static/js/2.4e2a8415.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todolist/static/js/2.4e2a8415.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44d77526e94612057472",
+    "url": "/react-todolist/static/js/main.958fe87b.chunk.js"
+  },
+  {
+    "revision": "2d0859b691aabd144816",
+    "url": "/react-todolist/static/js/runtime-main.4710b131.js"
+  }
+]);
